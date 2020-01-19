b158f702a5725874d87bf4d338b12140f1f05d9a
Kota Watanabe <kota3316@gmail.com>
2020-01-18 19:41:10 -0800

    partial application

--- functional_programming/currying.js
+++ functional_programming/currying.js
@@ -1,4 +1,10 @@
+//Currying
 const curriedMultiply = (a) => (b) => a*b;
 const curriedMultiplyBy5 = curriedMultiply(5);
 
 console.log(curriedMultiplyBy5(4))//20
+
+//Partial Application
+const multiply = (a,b,c) => a*b*c;
+const partialMultiplyBy5 = multiply.bind(null,5)
+console.log(partialMultiplyBy5(4,10));
