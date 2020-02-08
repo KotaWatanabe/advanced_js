5ccf8722744224c5f58ed353b8495ce3e4b75d3a
Kota Watanabe <kota3316@gmail.com>
2020-02-08 10:11:29 -0800

    recursion fibonacci and reverse string

---
+++ algorithms/recursion/fibonacci.js
@@ -0,0 +1,19 @@
+function fibonacciIterative(n){
+    let arr = [0,1];
+    for(let i = 2; i <= n; i++) {
+        arr.push((arr[i-1])+(arr[i-2]));
+    }
+    return arr[n];
+}
+console.log(fibonacciIterative(8));
+  
+function fibonacciRecursive(n) {
+   if(n < 2) {
+    return n;
+   } 
+   return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+}
+  
+console.log(fibonacciRecursive(8))
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
