3b669ed7b26e2b3974ba2e59f00f6ea0b8f6ae0c
Kota Watanabe <kota3316@gmail.com>
2020-02-07 21:31:38 -0800

    factorial recursion

---
+++ algorithms/recursion/factorial.js
@@ -0,0 +1,17 @@
+function findFactorialRecursive(number) {
+    if(number === 2) {
+        return 2;
+    }
+    return number * findFactorialRecursive(number-1);
+}
+  
+function  findFactorialIterative(number) {
+  let answer = 1;
+  for(let i = number; i > 0; i--) {
+      answer *= i;
+  }
+  return answer;
+}
+  
+console.log(findFactorialRecursive(5));
+console.log(findFactorialIterative(5));
