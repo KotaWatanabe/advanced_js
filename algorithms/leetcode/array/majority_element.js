3b03ec599a38ce00cd38207540bf888559ca862a
Kota Watanabe <kota3316@gmail.com>
2020-02-20 12:12:26 -0800

    clean way

--- algorithms/leetcode/array/majority_element.js
+++ algorithms/leetcode/array/majority_element.js
@@ -29,5 +29,11 @@ var majorityElement = function(nums) {
     return maxKey;
 };
 
+var majorityElement = function(nums) {
+    nums.sort();
+    const i = Math.floor(nums.length/2);
+    return nums[i];
+};
+
 console.log(majorityElement([3,2,3]));
 console.log(majorityElement([2,2,1,1,1,2,2]));
